5339aa764fa132d9d94636ffb2bf1aec20433d5c
my name <myemailid@domain.com>
2022-05-06 22:16:07 -0400

    added nothing

--- index.js
+++ index.js
@@ -32,12 +32,13 @@ function verifyJWT(req, res, next) {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nnong.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
         const orderCollection = client.db('geniusCar').collection('order');
+// console.log('working');
+
 
 
         //AUTH
